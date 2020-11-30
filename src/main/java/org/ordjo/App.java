d2e6918f7ae36619aa5d1dfdb1db201ec0d0de2b
Denis Ponizhan <denis.ponizhan@gmail.com>
2020-11-30 17:05:10 +0200

    add configuration to deploy to tomcat

--- src/main/java/org/ordjo/App.java
+++ src/main/java/org/ordjo/App.java
@@ -2,15 +2,23 @@ package org.ordjo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    //this is needed to deploy to tomcat server
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 
     @RequestMapping("/")
