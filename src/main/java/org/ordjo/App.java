6dc8abd59b00fec752145b8bf489b5c2e72079e7
Denis Ponizhan <denis.ponizhan@gmail.com>
2020-11-30 15:05:25 +0200

    add jsp

--- src/main/java/org/ordjo/App.java
+++ src/main/java/org/ordjo/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
@@ -15,8 +14,7 @@ public class App {
     }
 
     @RequestMapping("/")
-    @ResponseBody
     String home() {
-        return "Hello, Home!";
+        return "home";
     }
 }
