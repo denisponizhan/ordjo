b0e8d730227326804dfa3d0b075bb35368c3b2a0
Denis Ponizhan <denis.ponizhan@gmail.com>
2020-12-10 17:52:31 +0200

    add first entity with JPA annotations

---
+++ src/main/java/org/ordjo/model/StatusUpdate.java
@@ -0,0 +1,75 @@
+package org.ordjo.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="status_update")
+public class StatusUpdate {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "added")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date added;
+
+    public StatusUpdate(String text) {
+        this.text = text;
+    }
+
+    public StatusUpdate(String text, Date added) {
+        this.text = text;
+        this.added = added;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getAdded() {
+        return added;
+    }
+
+    public void setAdded(Date added) {
+        this.added = added;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        if (added == null) {
+            added = new Date();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StatusUpdate that = (StatusUpdate) o;
+        return Objects.equals(id, that.id) && Objects.equals(text, that.text) && Objects.equals(added, that.added);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text, added);
+    }
+}
